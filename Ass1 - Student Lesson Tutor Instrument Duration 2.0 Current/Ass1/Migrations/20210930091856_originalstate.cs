17c56c24dc774892a067da73f9bfdb5ac46700c6
Lucas <48338981+LucasGitCode@users.noreply.github.com>
2021-10-28 10:11:30 +0930

    Assignment 2 Group work

---
+++ Ass1 - Student Lesson Tutor Instrument Duration 2.0 Current/Ass1/Migrations/20210930091856_originalstate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ass1.Migrations
+{
+    public partial class originalstate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Letter",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Letter", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Letter_Student_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Letter_StudentsId",
+                table: "Letter",
+                column: "StudentsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Letter");
+        }
+    }
+}
