90feec82b8eca6d401c350df88d969d1de378aaa
Lucas <48338981+LucasGitCode@users.noreply.github.com>
2021-11-10 13:49:17 +0930

    Music App
    Github is confusing

---
+++ Ass1 - Student Lesson Tutor Instrument Duration 4.0 Current/Ass1/Migrations/20211110010827_deletelettermodel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ass1.Migrations
+{
+    public partial class deletelettermodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Letter");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Letter",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DurationCostId = table.Column<int>(type: "int", nullable: false),
+                    Signature = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentsId = table.Column<int>(type: "int", nullable: false),
+                    paid = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Letter", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Letter_DurationCost_DurationCostId",
+                        column: x => x.DurationCostId,
+                        principalTable: "DurationCost",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Letter_Student_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Letter_DurationCostId",
+                table: "Letter",
+                column: "DurationCostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Letter_StudentsId",
+                table: "Letter",
+                column: "StudentsId");
+        }
+    }
+}
